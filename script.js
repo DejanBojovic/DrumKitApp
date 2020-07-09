533d261c341923e4f477812c4f8978e9eadf4472
DejanBojovic <dejan.bojovic88gmail.com>
2020-07-09 14:40:58 +0200

    fixing animation on the keys

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
-const main = document.querySelector('.main');
-const button = document.querySelector('#signature');
+// you need to press musical-note button in order to acces keys 
+const keysDiv = document.querySelector(".keys"); // keys section
+const button = document.querySelector(".signature"); // button with musical note
 
+// before pressing the button all the keys are invisible
+// after pressing button becomes invisible and keys are showed
 button.addEventListener('click', function(){
-    main.style.visibility = 'visible';
-    button.style.display = 'none';
-            
+    keysDiv.style.visibility = "visible";
+    button.style.display = "none";      
 });
 
+// function that gathers information about which key user presses and then plays the appropriate sound
 function playSound(e) {
+    // getting the audio element with keyCode of the pressed key
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-            
-    if(!audio) return; // stop function from running 
+    // getting the div element that is visual representation of the pressed key
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+     
+    // if audio's keyCode is not connected to one of the pressed letters it is assigned to null and function should be stopped
+    if(!audio) {
+        return;
+    }
 
-    audio.currentTime = 0; // rewinds audio to start
-    audio.play(); // plays audio
-
+    // rewinds audio to start in order not to glitch when user presses the key multiple times fast
+    audio.currentTime = 0;
+    // plays audio connected to the key
+    audio.play();
+    // adding class "playing" to the pressed key for the scale and color change effects
     key.classList.add('playing');
+
+    // removing class "playing" after 0.2s because that is when transition finishes
+    setTimeout(function() {
+        key.classList.remove("playing");
+    }, 200)
 }
 
-function removeTransition(e) {
-    if(e.propertyName !== 'transform') return; // skip if its not a transform
-
-    this.classList.remove('playing');
-}
-
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-
+// adding event listener on window object to listen for keypress of the user
 window.addEventListener('keydown', playSound);
